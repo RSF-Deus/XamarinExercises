60d76f549297d190d6d6d942e8f34a090dc890a9
Mehdi Alfiguigui <MAF@dns-corim.com>
2019-01-15 12:23:43 +0100

    sauvegarde avant midi

--- MAF.LearnMobile/MAF.LearnMobile.XamarinAndroid_1/MainActivity.cs
+++ MAF.LearnMobile/MAF.LearnMobile.XamarinAndroid_1/MainActivity.cs
@@ -6,14 +6,20 @@ using Android.Widget;
 
 namespace MAF.LearnMobile.XamarinAndroid_1
 {
-    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    [Activity( Label = "@string/app_name" , Theme = "@style/AppTheme" , MainLauncher = true )]
     public class MainActivity : AppCompatActivity
     {
-        protected override void OnCreate(Bundle savedInstanceState)
+        protected override void OnCreate ( Bundle savedInstanceState )
         {
-            base.OnCreate(savedInstanceState);
+            base.OnCreate( savedInstanceState );
             // Set our view from the "main" layout resource
-            SetContentView(Resource.Layout.activity_main);
+            SetContentView( Resource.Layout.activity_main );
+
+            // New code
+            EditText phoneNumberText = FindViewById<EditText>( Resource.Id.PhoneNumberText );
+            TextView translatedPhoneWord = FindViewById<TextView>( Resource.Id.TranslatedPhoneword );
+            Button translateButton = FindViewById<Button>( Resource.Id.TranslateButton );
+            
         }
     }
 }
